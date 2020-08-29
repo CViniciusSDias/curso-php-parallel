e1b965a3ee397fe38bcdadc9cf8abaf9a033dcce
Vinicius Dias <carlosv775@gmail.com>
2020-08-29 14:00:58 -0300

    Vídeo 2.4

--- resize-images.php
+++ resize-images.php
@@ -11,23 +11,30 @@ $studentList = $repository->all();
 
 $runtimes = [];
 foreach ($studentList as $i => $student) {
-    $runtimes[$i] = new Runtime();
-    $runtimes[$i]->run(function (Student  $student) {
+    $runtimes[$i] = new Runtime(__DIR__ . '/vendor/autoload.php');
+    $runtimes[$i]->run(function (Student $student) {
         echo 'Resizing ' . $student->fullName() . ' profile picture' . PHP_EOL;
 
-        [$width, $height] = getimagesize($student->profilePicturePath());
+        $profilePicturePath = $student->profilePicturePath();
+        [$width, $height] = getimagesize($profilePicturePath);
 
         $ratio = $height / $width;
 
         $newWidth = 200;
         $newHeight = 200 * $ratio;
 
-        $sourceImage = imagecreatefromjpeg($student->profilePicturePath());
+        $sourceImage = imagecreatefromjpeg($profilePicturePath);
         $destinationImage = imagecreatetruecolor($newWidth, $newHeight);
         imagecopyresampled($destinationImage, $sourceImage, 0, 0, 0, 0, $newWidth, $newHeight, $width, $height);
 
-        imagejpeg($destinationImage, __DIR__ . '/storage/resized/' . basename($student->profilePicturePath()));
-    }, [$student]);
+        imagejpeg($destinationImage, __DIR__ . '/storage/resized/' . basename($profilePicturePath));
 
-    echo 'Finished resizing ' . $student->fullName() . ' profile picture' . PHP_EOL;
+        echo 'Finished resizing ' . $student->fullName() . ' profile picture' . PHP_EOL;
+    }, [$student]);
 }
+
+foreach ($runtimes as $runtime) {
+    $runtime->close();
+}
+
+echo 'Finalizando thread principal' . PHP_EOL;
