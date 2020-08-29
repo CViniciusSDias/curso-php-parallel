cb8cb5bf1e7a3e990a6310c8566f815b4001c3a5
Vinicius Dias <carlosv775@gmail.com>
2020-08-29 15:52:12 -0300

    Vídeo 4.1

--- calculate-points.php
+++ calculate-points.php
@@ -3,6 +3,7 @@
 use Alura\Threads\Activity\Activity;
 use Alura\Threads\Student\InMemoryStudentRepository;
 use Alura\Threads\Student\Student;
+use parallel\Channel;
 use parallel\Runtime;
 
 require_once 'vendor/autoload.php';
@@ -13,12 +14,13 @@ $studentList = $repository->all();
 $totalPoints = 0;
 $runtimes = [];
 $futures = [];
+$channel = Channel::make('points');
 foreach ($studentList as $i => $student) {
     $activities = $repository->activitiesInADay($student);
 
     $runtimes[$i] = new Runtime(__DIR__ . '/vendor/autoload.php');
 
-    $futures[$i] = $runtimes[$i]->run(function (array $activities, Student $student) {
+    $futures[$i] = $runtimes[$i]->run(function (array $activities, Student $student, Channel $channel) {
         $points = array_reduce(
             $activities,
             fn (int $total, Activity $activity) => $total + $activity->points(),
@@ -27,8 +29,12 @@ foreach ($studentList as $i => $student) {
 
         printf('%s made %d points today%s', $student->fullName(), $points, PHP_EOL);
 
+        $channel->send($points);
+
+        echo 'Valor enviado';
+
         return $points;
-    }, [$activities, $student]);
+    }, [$activities, $student, $channel]);
 }
 
 foreach ($futures as $future) {
