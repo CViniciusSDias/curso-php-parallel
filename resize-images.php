2ef274bca9188bd41cb94913a73a558d6f2f644d
Vinicius Dias <carlosv775@gmail.com>
2020-08-29 13:49:04 -0300

    Vídeo 2.3

--- resize-images.php
+++ resize-images.php
@@ -1,32 +1,33 @@
 <?php
 
 use Alura\Threads\Student\InMemoryStudentRepository;
+use Alura\Threads\Student\Student;
+use parallel\Runtime;
 
 require_once 'vendor/autoload.php';
 
 $repository = new InMemoryStudentRepository();
 $studentList = $repository->all();
 
-foreach ($studentList as $student) {
-    echo 'Resizing ' . $student->fullName() . ' profile picture' . PHP_EOL;
+$runtimes = [];
+foreach ($studentList as $i => $student) {
+    $runtimes[$i] = new Runtime();
+    $runtimes[$i]->run(function (Student  $student) {
+        echo 'Resizing ' . $student->fullName() . ' profile picture' . PHP_EOL;
 
-    resizeTo200PixelsWidth($student->profilePicturePath());
+        [$width, $height] = getimagesize($student->profilePicturePath());
+
+        $ratio = $height / $width;
+
+        $newWidth = 200;
+        $newHeight = 200 * $ratio;
+
+        $sourceImage = imagecreatefromjpeg($student->profilePicturePath());
+        $destinationImage = imagecreatetruecolor($newWidth, $newHeight);
+        imagecopyresampled($destinationImage, $sourceImage, 0, 0, 0, 0, $newWidth, $newHeight, $width, $height);
+
+        imagejpeg($destinationImage, __DIR__ . '/storage/resized/' . basename($student->profilePicturePath()));
+    }, [$student]);
 
     echo 'Finished resizing ' . $student->fullName() . ' profile picture' . PHP_EOL;
-}
-
-function resizeTo200PixelsWidth($imagePath)
-{
-    [$width, $height] = getimagesize($imagePath);
-
-    $ratio = $height / $width;
-
-    $newWidth = 200;
-    $newHeight = 200 * $ratio;
-
-    $sourceImage = imagecreatefromjpeg($imagePath);
-    $destinationImage = imagecreatetruecolor($newWidth, $newHeight);
-    imagecopyresampled($destinationImage, $sourceImage, 0, 0, 0, 0, $newWidth, $newHeight, $width, $height);
-
-    imagejpeg($destinationImage, __DIR__ . '/storage/resized/' . basename($imagePath));
 }
